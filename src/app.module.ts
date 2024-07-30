c40d267da51a2efce290691bebf4fdb53da5f3f4
trikhanhhk <nguyentrikhanh123hk@gmail.com>
2024-07-30 17:12:41 +0700

    update auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { UsersModule } from './users/users.module'
 import { MongooseModule } from '@nestjs/mongoose'
 import { AuthModule } from './auth/auth.module'
 import { ConfigModule } from '@nestjs/config'
+import { APP_GUARD } from '@nestjs/core'
+import { JwtAuthGuard } from './auth/jwt.guard'
+import { RolesGuard } from './auth/roles.guard'
 
 @Module({
   imports: [
@@ -17,6 +20,17 @@ import { ConfigModule } from '@nestjs/config'
     AuthModule
   ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard
+    },
+
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard
+    }
+  ]
 })
 export class AppModule {}
---
+++ src/auth/roles.guard.ts
@@ -0,0 +1,29 @@
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
+import { Observable } from 'rxjs'
+import { ROLES_KEY } from 'src/decorator/roles.decorator'
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass()
+    ])
+
+    if (!requiredRoles) {
+      return true
+    }
+
+    const request = context.switchToHttp().getRequest()
+
+    const user = request.user
+
+    if (!user || !requiredRoles.some((role) => user.roles.includes(role))) {
+      throw new ForbiddenException('You do not have the required role')
+    }
+
+    return true
+  }
+}
